362fe305281c3f2b7b01b208a367ce3f16e3110a
Héctor Rdz <hd.rdz98@gmail.com>
2021-05-14 09:37:56 -0500

    JumpBack base

---
+++ src/main/java/me/sasukector/custom_enchantments/enchants/JumpBack.java
@@ -0,0 +1,60 @@
+package me.sasukector.custom_enchantments.enchants;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.NamespacedKey;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class JumpBack extends Enchantment {
+
+    public JumpBack(String namespace) {
+        super(NamespacedKey.minecraft(namespace));
+    }
+
+    @Override
+    public @NotNull String getName() {
+        return "JumpBack";
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
+
+    @Override
+    public @NotNull EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.BOW;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(@NotNull Enchantment other) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(@NotNull ItemStack item) {
+        return false;
+    }
+
+    @Override
+    public @NotNull Component displayName(int level) {
+        return null;
+    }
+}
